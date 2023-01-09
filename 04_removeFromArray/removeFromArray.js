00f38df5968db0e2c2337ef12ef0089df9320373
Aaron Kendell <aaronkendell@gmail.com>
2023-01-09 10:42:20 -0700

    Finish removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
 
+    let result = [];
+
+    array.forEach((element) => {
+        if (!args.includes(element)) {
+            result.push(element);
+        }
+    });
+
+    return result;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
