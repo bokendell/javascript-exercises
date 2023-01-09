082cb6c8dc07562862d3f63d3fa3adbca2ec6a67
Aaron Kendell <aaronkendell@gmail.com>
2023-01-09 09:46:57 -0700

    Finish reverseString exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result = "";
+    for (let i = word.length; i >= 0; i--){
+        result += word.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
