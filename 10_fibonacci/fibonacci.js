24c5a88b42b827d4cede0363672e6e423c0e5e72
Aaron Kendell <aaronkendell@gmail.com>
2023-01-09 16:06:33 -0700

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    let num = parseInt(val);
+    if (num < 0) {
+        return "OOPS"
+    }
+    let arr = Array(num).fill(1);
+    for (let i = 2; i < num; i++){
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[num - 1];
 };
 
 // Do not edit below this line
