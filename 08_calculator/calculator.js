2b99ea474877a6f43f03af088a0fce9e620028fc
Aaron Kendell <aaronkendell@gmail.com>
2023-01-09 12:48:07 -0700

    Finish calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+	for (let i = 0; i < array.length; i++){
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array[0];
+	for (let i = 1; i < array.length; i++){
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let result = num;
+  for (let i = 1; i < power; i++){
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+	result = num;
+  for (let i = num - 1; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
