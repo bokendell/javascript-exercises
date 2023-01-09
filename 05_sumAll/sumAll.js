c53f154f0325848291432d21d98b44d4e8a98862
Aaron Kendell <aaronkendell@gmail.com>
2023-01-09 11:50:48 -0700

    Finish sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(par1, par2) {
 
+    if ((!Number.isInteger(par1)) || (!Number.isInteger(par2))){
+        return "ERROR";
+    }
+
+    if ((par1 < 0) || (par2 < 0)){
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    if (par1 < par2){
+        let start = par1;
+        let end = par2;
+
+        for (let i = par1; i <= par2; i++){
+            result += i;
+        }
+    }
+
+    else {
+        let start = par2;
+        let end = par1;
+        for (let i = par2; i <= par1; i++){
+            result += i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
