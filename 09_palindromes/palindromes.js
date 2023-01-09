a18bf2b8f06fc89dfef0cfabd3f84d7684461925
Aaron Kendell <aaronkendell@gmail.com>
2023-01-09 15:50:11 -0700

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^A-Za-z]/g, '');
+    string = string.toLowerCase();
+    let reverseWord = "";
+    for (let i = string.length; i >= 0; i--){
+        reverseWord += string.charAt(i);
+    }
+    if (string === reverseWord){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
